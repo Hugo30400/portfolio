f5c09fba8ae5124f8affdbfdbc399a0b455879fd
Mackbook Hugo <hugo.faus@etu.umontpellier.fr>
2024-12-17 13:26:16 +0100

    navbar animation OK

--- scripts/menu.js
+++ scripts/menu.js
@@ -7,3 +7,32 @@ document.addEventListener('DOMContentLoaded', function() {
         menu.classList.toggle('open');
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navbar = document.querySelector('.navbar');
+    const header = document.querySelector('header');
+    let lastScrollY = window.scrollY;
+
+    window.addEventListener('scroll', () => {
+        // Obtenir la position et la hauteur du header
+        const headerHeight = header.offsetHeight;
+        const currentScroll = window.scrollY;
+
+        // Si on a dépassé le header
+        if (currentScroll > headerHeight) {
+            // Gestion de l'affichage/masquage basé sur la direction du scroll
+            if (currentScroll > lastScrollY) {
+                navbar.classList.add('hide-nav');
+            } else {
+                navbar.classList.remove('hide-nav');
+            }
+            navbar.classList.add('nav-scrolled');
+        } else {
+            // Si on est dans le header
+            navbar.classList.remove('nav-scrolled');
+            navbar.classList.remove('hide-nav');
+        }
+
+        lastScrollY = currentScroll;
+    });
+});
